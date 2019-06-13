a0b032c9967e74540269623e7de23d8cce2fa2d2
Student <example@example.com>
2019-06-13 12:18:46 -0700

    add .pairs template, output handling to js, some css for padding and hiding output, and html for title and main heading

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
+  $("#some-form").submit(function(event) {
     event.preventDefault();
+
+    var someInputVar = $("#some-input").val(); // Don't forget it's a string without parseInt()
+    var someOutputVar = someInputVar;
+
+    $("#output").text(someOutputVar);
+    $("#output").show();
+  });
+});
